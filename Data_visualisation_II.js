d867f7fad3c76e4654499a7d95b0a3a9b22a2698
TerrenceSiau <tsia0006@studnt.monash.edu>
2025-10-17 10:38:39 +0800

    Did modification in terms of arragement and css

--- Data_visualisation_II.js
+++ Data_visualisation_II.js
@@ -1,5 +1,11 @@
 var vg_1 = "Data_visualisation_II.vg.json"
+var vg_2 = "Age_distribution.vg.json"
 
 vegaEmbed("#Visualisation", vg_1).then(function(result){
 
 }).catch(console.error);
+
+vegaEmbed("#age-distribution", vg_2).then(function(result){
+
+}).catch(console.error);
+
