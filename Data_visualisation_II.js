9539863fb839dc2d7ca44b9311c8ed82e4ae5620
TerrenceSiau <tsia0006@studnt.monash.edu>
2025-10-18 13:04:19 +0800

    Restruture the file and layout of visualisation

--- Data_visualisation_II.js
+++ Data_visualisation_II.js
@@ -1,11 +1,21 @@
 var vg_1 = "Data_visualisation_II.vg.json"
 var vg_2 = "Age_distribution.vg.json"
+var vg_3 = "State_density.vg.json"
+var vg_4 = "Gender_ethnics_structure.vg.json"
 
 vegaEmbed("#Visualisation", vg_1).then(function(result){
 
 }).catch(console.error);
 
 vegaEmbed("#age-distribution", vg_2).then(function(result){
+
+}).catch(console.error);
+
+vegaEmbed("#state-density", vg_3).then(function(result){
+
+}).catch(console.error);
+
+vegaEmbed("#gender-ethnics", vg_4).then(function(result){
 
 }).catch(console.error);
 
